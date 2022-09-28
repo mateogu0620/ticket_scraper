7da65f9aeceed4e04e7f1c35a473fa89c477bd04
mateogu0620 <mateogarcia0620@gmail.com>
2022-09-28 17:28:11 -0400

    eventbrite update and cleaning up scraper setup

--- scraper/scraper.py
+++ scraper/scraper.py
@@ -6,27 +6,23 @@ API Keys (will eventually be stored as env variables so hopefully nobody else lo
 Docs: 
     - https://developer.ticketmaster.com/products-and-docs/apis/discovery-api/v2/#search-events-v2
     - https://platform.seatgeek.com/
-    -
 
+Eventbrite no longer allows for searching events since December 12, 2019
 '''
 
 # Ticketmaster
 TICKETMASTER_API_KEY = "W41W8WQEjosIaqHVjoWJGDDdFbB9Begq"
 # SeatGeek
-SEATGEEK_CLIENTID = "Mjk0MDI2MjJ8MTY2NDM5OTMyMi40MTQzNTQ"
-SEATGEEK_SECRET = "ef42c7b4250a963a18a8f42fcb817bcdc3c2889e8740ac276b6834f48df45275"
-SEATGEEK_AUTH = f"client_id={SEATGEEK_CLIENTID}&client_secret={SEATGEEK_SECRET}"
+SEATGEEK_API_KEY = "Mjk0MDI2MjJ8MTY2NDM5OTMyMi40MTQzNTQ"
+SEATGEEK_API_SECRET = "ef42c7b4250a963a18a8f42fcb817bcdc3c2889e8740ac276b6834f48df45275"
 
 # Examples for searching events using all three APIs - refer to docs for more info
 testTMQuery = f"https://app.ticketmaster.com/discovery/v2/events?apikey={TICKETMASTER_API_KEY}&locale=*&size=1"
 testSGQuery = f"https://api.seatgeek.com/2/events?geoip=11201"
 
-print(testSGQuery)
-
-
 
 responseTM = get(testTMQuery)
-responseSG = get(testSGQuery, auth=(SEATGEEK_CLIENTID, SEATGEEK_SECRET))
+responseSG = get(testSGQuery, auth=(SEATGEEK_API_KEY, SEATGEEK_API_SECRET))
 
 print(responseTM.json())
 print(responseSG.json())
