e9afd1a2c5cb794e12c1b8871fb2203e5425f60c
mateogu0620 <mateogarcia0620@gmail.com>
2022-09-28 17:17:10 -0400

    added seatgeek API setup and basic usage

--- scraper/scraper.py
+++ scraper/scraper.py
@@ -1,9 +1,32 @@
 from requests import get
 
-# API Keys (will eventually be stored as env variables so hopefully nobody else looks at this repo please?)
+'''
+API Keys (will eventually be stored as env variables so hopefully nobody else looks at this repo please?)
+
+Docs: 
+    - https://developer.ticketmaster.com/products-and-docs/apis/discovery-api/v2/#search-events-v2
+    - https://platform.seatgeek.com/
+    -
+
+'''
+
+# Ticketmaster
 TICKETMASTER_API_KEY = "W41W8WQEjosIaqHVjoWJGDDdFbB9Begq"
+# SeatGeek
+SEATGEEK_CLIENTID = "Mjk0MDI2MjJ8MTY2NDM5OTMyMi40MTQzNTQ"
+SEATGEEK_SECRET = "ef42c7b4250a963a18a8f42fcb817bcdc3c2889e8740ac276b6834f48df45275"
+SEATGEEK_AUTH = f"client_id={SEATGEEK_CLIENTID}&client_secret={SEATGEEK_SECRET}"
 
-testQuery = f"https://app.ticketmaster.com/discovery/v2/events?apikey={TICKETMASTER_API_KEY}&locale=*&size=1"
-response = get(testQuery)
+# Examples for searching events using all three APIs - refer to docs for more info
+testTMQuery = f"https://app.ticketmaster.com/discovery/v2/events?apikey={TICKETMASTER_API_KEY}&locale=*&size=1"
+testSGQuery = f"https://api.seatgeek.com/2/events?geoip=11201"
 
-print(response.json())
+print(testSGQuery)
+
+
+
+responseTM = get(testTMQuery)
+responseSG = get(testSGQuery, auth=(SEATGEEK_CLIENTID, SEATGEEK_SECRET))
+
+print(responseTM.json())
+print(responseSG.json())
