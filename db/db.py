70675ad09b6eee9cb5cb41fb89181a89d57b0269
narensrinivasan <85373920+narensrinivasan@users.noreply.github.com>
2022-09-28 21:28:44 -0400

    Added API endpoint and sample POST request
    Also includes important info in text file (i know, bad practice)

--- db/db.py
+++ db/db.py
@@ -4,9 +4,38 @@ At first, it will just contain stubs that return fake data.
 Gradually, we will fill in actual calls to our datastore.
 """
 
+import json
+import requests
 
 def fetch_pets():
     """
     A function to return all pets in the data store.
     """
     return {"tigers": 2, "lions": 3, "zebras": 1}
+
+def api_post():
+    url = "https://data.mongodb-api.com/app/data-gvhux/endpoint/data/v1/action/findOne"
+
+    payload = json.dumps({
+        "collection": "people",
+        "database": "gettingStarted",
+        "dataSource": "Cluster08493",
+        "projection": {
+            "_id": 1
+        }
+    })
+    headers = {
+    'Content-Type': 'application/json',
+    'Access-Control-Request-Headers': '*',
+    'api-key': 'Gyq0EYJvvC3z2bxJIY6b46HunN6LfqYlpXycSLEXPOYO77zOGmNvIRIUsQSqp44Y', 
+    }
+
+    response = requests.request("POST", url, headers=headers, data=payload)
+
+    print(response.text)
+
+api_post()
+
+
+
+    
