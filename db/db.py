1b5f05970ba7b93038822b518d2e0c86c129a9b4
narensrinivasan <85373920+narensrinivasan@users.noreply.github.com>
2022-09-28 21:54:23 -0400

    Added additional methods to db.py for POST API requests
    Can now find, insert, and delete. Still adding customization.

--- db/db.py
+++ db/db.py
@@ -6,6 +6,7 @@ Gradually, we will fill in actual calls to our datastore.
 
 import json
 import requests
+import datetime
 
 def fetch_pets():
     """
@@ -13,15 +14,15 @@ def fetch_pets():
     """
     return {"tigers": 2, "lions": 3, "zebras": 1}
 
-def api_post():
+def api_findOne(first, last):
     url = "https://data.mongodb-api.com/app/data-gvhux/endpoint/data/v1/action/findOne"
 
     payload = json.dumps({
         "collection": "people",
         "database": "gettingStarted",
         "dataSource": "Cluster08493",
-        "projection": {
-            "_id": 1
+        'filter' : {
+            "name": {"first" : first, "last" : last}
         }
     })
     headers = {
@@ -34,7 +35,61 @@ def api_post():
 
     print(response.text)
 
-api_post()
+def api_insertOne():
+    url = "https://data.mongodb-api.com/app/data-gvhux/endpoint/data/v1/action/insertOne"
+
+    payload = json.dumps({
+        "collection": "people",
+        "database": "gettingStarted",
+        "dataSource": "Cluster08493",
+        "document" : {
+            "name" : {
+                "first" : "Walter",
+                "last" : "White"
+            },
+            "birth": json.dumps(datetime.datetime.now(), indent=4, sort_keys=True, default=str),
+            "death": json.dumps(datetime.datetime.now(), indent=4, sort_keys=True, default=str),
+            "contribs" : [ "Jesse", "We", "Need", "To", "Cook"],
+            "views" : 5000000
+        }
+    })
+    headers = {
+    'Content-Type': 'application/json',
+    'Access-Control-Request-Headers': '*',
+    'api-key': 'Gyq0EYJvvC3z2bxJIY6b46HunN6LfqYlpXycSLEXPOYO77zOGmNvIRIUsQSqp44Y', 
+    }
+
+    response = requests.request("POST", url, headers=headers, data=payload)
+
+    print(response.text)
+
+def api_deleteOne():
+    url = "https://data.mongodb-api.com/app/data-gvhux/endpoint/data/v1/action/deleteOne"
+
+    payload = json.dumps({
+        "collection": "people",
+        "database": "gettingStarted",
+        "dataSource": "Cluster08493",
+        "filter" : {"name" : {"first": "Walter", "last": "White"}}
+    })
+    headers = {
+    'Content-Type': 'application/json',
+    'Access-Control-Request-Headers': '*',
+    'api-key': 'Gyq0EYJvvC3z2bxJIY6b46HunN6LfqYlpXycSLEXPOYO77zOGmNvIRIUsQSqp44Y', 
+    }
+
+    response = requests.request("POST", url, headers=headers, data=payload)
+
+    print(response.text)
+
+
+api_findOne("Alan", "Turing")
+api_findOne("Walter", "White")
+api_deleteOne()
+api_findOne("Alan", "Turing")
+api_findOne("Walter", "White")
+
+
 
 
 
