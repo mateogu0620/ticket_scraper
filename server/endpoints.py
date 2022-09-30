2e26698972e9a4ea94aa91c88d63d6f0752e501c
narensrinivasan <85373920+narensrinivasan@users.noreply.github.com>
2022-09-30 14:24:47 -0400

    formatting in endpoints.py

--- server/endpoints.py
+++ server/endpoints.py
@@ -9,9 +9,11 @@ from pymongo import MongoClient
 
 app = Flask(__name__)
 api = Api(app)
-client = MongoClient('localhost', 27017) # Username: Cluster08493 Password: RlhRbmFKSH1E
 
-db = client.flask_db # creating mongoDB instance 
+client = MongoClient('localhost', 27017)
+# Username: Cluster08493
+# Password: RlhRbmFKSH1E
+db = client.flask_db  # creating mongoDB instance
 collection = db.collec
 
 LIST = 'list'
