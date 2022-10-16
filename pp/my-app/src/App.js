1e2b37880dcd1177591e683e134bbe76dfaafa2b
mateogu0620 <mateogarcia0620@gmail.com>
2022-10-16 18:17:01 -0400

    my_app back in pp folder

---
+++ pp/my-app/src/App.js
@@ -0,0 +1,25 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Ticket Scraper</h1>
+        <table>
+        <tr>
+        <td>Event Name</td>
+        <td>Location</td>
+        <td>Date</td>
+        <td>Price</td>
+        <td>Venue</td>
+        <td>Artist</td>
+        <td>Genre</td>
+        <td>Ticket Availability</td>   
+        </tr>
+        </table>
+      </header>
+    </div>
+  );
+}
+
+export default App;
