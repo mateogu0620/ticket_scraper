d627e17e9c405daca111180663c869906320a740
NiravGolyalla <ng2411@nyu.edu>
2022-10-23 22:57:40 -0400

    React App update
    Setup for the table of querys

---
+++ pp/app/src/App.js
@@ -0,0 +1,58 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Ticket Scraper</h1>
+        <table id="customers">
+        <tr>
+          <th>Event Name</th>
+          <th>Location</th>
+          <th>Date</th>
+          <th>Price</th>
+          <th>Venue</th>
+          <th>Artist</th>
+          <th>Genre</th>
+          <th>Ticket Availability</th>
+        </tr>
+        
+        <tr>
+          <td></td>
+          <td></td>
+          <td></td>
+          <td></td>
+          <td></td>
+          <td></td>
+          <td></td>
+          <td></td>
+        </tr>
+        <tr>
+          <td></td>
+          <td></td>
+          <td></td>
+          <td></td>
+          <td></td>
+          <td></td>
+          <td></td>
+          <td></td>
+        </tr>
+        <tr>
+          <td></td>
+          <td></td>
+          <td></td>
+          <td></td>
+          <td></td>
+          <td></td>
+          <td></td>
+          <td></td>
+        </tr>
+      </table>
+        
+        
+      </header>
+    </div>
+  );
+}
+
+export default App;
