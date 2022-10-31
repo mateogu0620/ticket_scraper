a29e90c4c3b029efb656c9e5f3061d72e1727a37
NiravGolyalla <ng2411@nyu.edu>
2022-10-30 23:47:04 -0400

    Css for paragraph

--- pp/app/src/App.js
+++ pp/app/src/App.js
@@ -49,9 +49,7 @@ function App() {
         </tr>
       </table>
         
-        <p>About: This event searcher searches for nearby events in the area. The options given to filter the events by Location, Date, Price, Venue, Artist, Genre, and Ticket Availability. 
-          This project was done to make nearby, local events more accessable to visitors, tourists , and people that are bored and are looking for fun things to do in the area. This website 
-          searches using SeatGeek and Ticketmaster to provide a streamline and one stop shop for events near you.
+        <p>About: This event searcher searches for nearby events in the area. The options given to filter the events by Location, Date, Price, Venue, Artist, Genre, and Ticket Availability. This project was done to make nearby, local events more accessable to visitors, tourists , and people that are bored and are looking for fun things to do in the area. This website searches using SeatGeek and Ticketmaster to provide a streamline and one stop shop for events near you.
 
 
         </p>
