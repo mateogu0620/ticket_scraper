2d22e319ed4aa0f6c37113063805180bcad109db
NiravGolyalla <ng2411@nyu.edu>
2022-12-04 23:05:00 -0500

    Display Test from Backend to React

--- pp/app/src/App.js
+++ pp/app/src/App.js
@@ -60,15 +60,20 @@ function App() {
         </tr>
       </table>
         
-        <p>About: This event searcher searches for nearby events in the area. The options given to filter the events by Location, Date, Price, Venue, Artist, Genre, and Ticket Availability. This project was done to make nearby, local events more accessable to visitors, tourists , and people that are bored and are looking for fun things to do in the area. This website searches using SeatGeek and Ticketmaster to provide a streamline and one stop shop for events near you.
-
-
-        </p>
+        <p>About: This event searcher searches for nearby events in the area. The options given to filter the events by Location, Date, Price, Venue, Artist, Genre, and Ticket Availability. This project was done to make nearby, local events more accessable to visitors, tourists , and people that are bored and are looking for fun things to do in the area. This website searches using SeatGeek and Ticketmaster to provide a streamline and one stop shop for events near you.</p>
 
       </header>
-
-
+      <div>
+        {(typeof data.events == 'undefined') ? (
+          <p>Error</p>
+        ):(
+          data.events.map((event,i) => (
+            <p key={i}>{event}</p>
+          ))
+        )}
+      </div>
     </div>
+
   );
 }
 
