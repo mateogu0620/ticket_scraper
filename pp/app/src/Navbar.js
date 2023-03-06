c724ae8aca7f4d07d7eeb1c179af5d833a9990ba
NiravGolyalla <ng2411@nyu.edu>
2023-03-05 23:23:29 -0500

    Attempting to add List elements(links to different pages))

--- pp/app/src/Navbar.js
+++ pp/app/src/Navbar.js
@@ -4,33 +4,33 @@ import React, {useState, useEffect, useRef} from 'react';
 
 function Navbar(){
     const [open, setOpen] = useState(false);
-    return <nav className="nav">
+    return (<nav className="nav">
         <a href="/" className="site-title">Ticket Master</a>
         <ul>
             <li>
                 <a href="/login">Login</a>
             </li>
             <li>
-                <div className='toggle' onClick={()=>{setOpen(!open)}}>Profile</div>
-                <div className={`DropDown ${open? 'active' : 'inactive'}`}>
-                <ul>
-                    <DropDownElement text = {"My Profile"}/>
-                </ul>
-                    
-                </div>
+                <div className='toggle' onClick={()=>{setOpen(!open)}}><p>Profile</p></div>
             </li>
             
         </ul>
-    </nav>
-
+        <div className={`DropDown ${open? 'active' : 'inactive'}`}>
+            <ul>
+            <DropDownElement text={"My Profile"} />
+            <DropDownElement text={"Edit Profile"} />
+            <DropDownElement text={"Log Out"} />
+            </ul>            
+        </div>
+    </nav>);
 }
 
 function DropDownElement(props){
-    return
-        <li className="dropDownItem">
+    return(
+        <li className="dropDownElement">
             <a>[props.text]</a>
         </li>
-    
+    );
 }
 
 export default Navbar;
