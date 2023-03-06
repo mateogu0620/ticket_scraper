2c8f58b590a6df939bd16e523cadab940c92beba
NiravGolyalla <ng2411@nyu.edu>
2023-03-05 22:36:18 -0500

    Adding Menu for profile

--- pp/app/src/Navbar.js
+++ pp/app/src/Navbar.js
@@ -1,10 +1,36 @@
-export default function Navbar(){
+import './App.css';
+
+import React, {useState, useEffect, useRef} from 'react';
+
+function Navbar(){
+    const [open, setOpen] = useState(false);
     return <nav className="nav">
         <a href="/" className="site-title">Ticket Master</a>
         <ul>
             <li>
                 <a href="/login">Login</a>
             </li>
+            <li>
+                <div className='toggle' onClick={()=>{setOpen(!open)}}>Profile</div>
+                <div className={`DropDown ${open? 'active' : 'inactive'}`}>
+                <ul>
+                    <DropDownElement text = {"My Profile"}/>
+                </ul>
+                    
+                </div>
+            </li>
+            
         </ul>
     </nav>
+
 }
+
+function DropDownElement(props){
+    return
+        <li className="dropDownItem">
+            <a>[props.text]</a>
+        </li>
+    
+}
+
+export default Navbar;
